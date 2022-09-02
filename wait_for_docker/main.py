10cf696fcef4d99ef99ced4740ca8a65b7db65e2
Goto Hayato <habita.gh@gmail.com>
2022-09-02 20:26:35 +0900

    Issue #7: Add `--version` option.

--- wait_for_docker/main.py
+++ wait_for_docker/main.py
@@ -1,20 +1,44 @@
 """Wait for Docker to be active."""
+import argparse
 import sys
 import time
 from contextlib import contextmanager
+from dataclasses import dataclass
 
 import docker
 from docker.errors import DockerException
 from yaspin import yaspin
 
+from wait_for_docker import __version__
+
 
 def main():
+    args = get_args()
+
+    if args.version:
+        print(__version__)
+        sys.exit()
+
     with exit_with_interrupt():
         with yaspin():
             while not check_docker():
                 time.sleep(0.5)
 
         print("Docker is active now.")
+
+
+@dataclass
+class CliArgs:
+    version: bool
+
+
+def get_args() -> CliArgs:
+    parser = argparse.ArgumentParser(__doc__)
+    parser.add_argument("--version", "-V", action="store_true")
+
+    parsed = parser.parse_args()
+
+    return CliArgs(version=parsed.version)
 
 
 def check_docker() -> bool:
